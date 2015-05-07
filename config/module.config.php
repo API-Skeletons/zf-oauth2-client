9e9de8de342dfb4c6e7cb230ee082bbd3b41148e
Tom Anderson <tanderson@solutionstream.com>
2015-05-07 10:48:18 -0600

    OAuth2 Client for zf-oauth2

---
+++ config/module.config.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'zf-oauth2-client' => array(
+        'default' => array(
+            'clientId' => '',
+            'secret' => '',
+            'endpoint' => 'http://localhost:8081/oauth',
+        ),
+    ),
+);
